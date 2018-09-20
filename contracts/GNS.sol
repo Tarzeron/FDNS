a602ba88edccecb035e1529a718b2f2dcf31206a
Yuriy <yuriy@gmail.com>
2018-09-20 13:12:18 +0300

    createrecord, check for unique

--- contracts/GNS.sol
+++ contracts/GNS.sol
@@ -9,6 +9,10 @@ contract GNS {
     mapping (address => string) private _namesOfOwner;
     mapping (bytes => uint128) private _existRawRecordsByContent;
     
+    constructor() public{
+        _records.length++;
+    }
+    
     modifier onlyOwnerOfName(string _name) {
         address owner = _ownerOfName[_name];
         require(owner == 0 || owner == msg.sender);
@@ -46,8 +50,8 @@ contract GNS {
     }
     
     function isValidFDNSRecord(bytes _rawRecord) pure public returns(bool) {
-        uint8 typeOfRcord = uint8(_rawRecord[0]);
-        if(typeOfRcord != 0)
+        uint8 typeOfRecord = uint8(_rawRecord[0]);
+        if(typeOfRecord != 0)
             return false;
         if(_rawRecord.length < 5)
             return false;
@@ -57,8 +61,8 @@ contract GNS {
     }
     
     function isValidIPv4Record(bytes _rawRecord) pure public returns(bool) {
-        uint8 typeOfRcord = uint8(_rawRecord[0]);
-        if(typeOfRcord != 1)
+        uint8 typeOfRecord = uint8(_rawRecord[0]);
+        if(typeOfRecord != 1)
             return false;
         if(_rawRecord.length != 5)
             return false;
@@ -66,8 +70,8 @@ contract GNS {
     }
     
     function isValidIPv6Record(bytes _rawRecord) pure public returns(bool) {
-        uint8 typeOfRcord = uint8(_rawRecord[0]);
-        if(typeOfRcord != 2)
+        uint8 typeOfRecord = uint8(_rawRecord[0]);
+        if(typeOfRecord != 2)
             return false;
         if(_rawRecord.length != 17)
             return false;
@@ -75,8 +79,8 @@ contract GNS {
     }
     
     function isValidDNSRecord(bytes _rawRecord) pure public returns(bool) {
-        uint8 typeOfRcord = uint8(_rawRecord[0]);
-        if(typeOfRcord != 3)
+        uint8 typeOfRecord = uint8(_rawRecord[0]);
+        if(typeOfRecord != 3)
             return false;
         if(!isValidString(_rawRecord, 1, uint32(_rawRecord.length - 1)))
             return false;
@@ -89,16 +93,16 @@ contract GNS {
     function isValidRecord(bytes _rawRecord) pure public returns(bool) {
         if(_rawRecord.length <= 1)
             return false;
-        uint8 typeOfRcord = uint8(_rawRecord[0]);
-        if(typeOfRcord >= 64 && typeOfRcord <= 255)
+        uint8 typeOfRecord = uint8(_rawRecord[0]);
+        if(typeOfRecord >= 64 && typeOfRecord <= 255)
             return true;
-        if(typeOfRcord == 0)
+        if(typeOfRecord == 0)
             return isValidFDNSRecord(_rawRecord);
-        else if(typeOfRcord == 1)
+        else if(typeOfRecord == 1)
             return isValidIPv4Record(_rawRecord);
-        else if(typeOfRcord == 2)
+        else if(typeOfRecord == 2)
             return isValidIPv6Record(_rawRecord);
-        else if(typeOfRcord == 3)
+        else if(typeOfRecord == 3)
             return isValidDNSRecord(_rawRecord);
         return false;
     }
@@ -113,11 +117,22 @@ contract GNS {
             bytes _rawRecord) 
             onlyOwnerOfName(_name)
             public {
-        uint128 recordIndex = uint128(_records.push(_rawRecord)-1);
-        _recordIdsForOwner[msg.sender].push(recordIndex) ;
-        
-        // _existRawRecordsByContent!!!!!!!!!!!
-        // _recordIdsForOwnerByType!!!!!!!!!!!
+        require(isValidRecord(_rawRecord));
+        uint128 recordIndex = _existRawRecordsByContent[_rawRecord];
+        uint8 typeOfRecord = uint8(_rawRecord[0]);
+        if(recordIndex>0){
+            uint128[] memory recByType = _recordIdsForOwnerByType[msg.sender][typeOfRecord];
+            bytes32 hash = keccak256(_rawRecord);
+            for(uint128 i=0;i<recByType.length;i++)
+                if(keccak256(_records[recByType[i]]) == hash)
+                    revert();
+        }
+        if (recordIndex==0) {
+            recordIndex = uint128(_records.push(_rawRecord)-1);
+            _existRawRecordsByContent[_rawRecord] =recordIndex;
+        }
+        _recordIdsForOwner[msg.sender].push(recordIndex);
+        _recordIdsForOwnerByType[msg.sender][typeOfRecord].push(recordIndex);
     }
     
     function removeRecordByIndex(uint128 _recordIndex) public {
